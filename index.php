51f9af9eb79105d3d9f5780932d02d5fe4dd51e1
Rinacoder <74737743+Rinacoder@users.noreply.github.com>
2023-08-13 04:46:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+	<link href="styles.css" rel="stylesheet">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+	<!-- Шрифты -->
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,300&display=swap" rel="stylesheet">
+	<script type="text/javascript">
+		(function(_,r,e,t,a,i,l){_['retailCRMObject']=a;_[a]=_[a]||function(){(_[a].q=_[a].q||[]).push(arguments)};_[a].l=1*new Date();l=r.getElementsByTagName(e)[0];i=r.createElement(e);i.async=!0;i.src=t;l.parentNode.insertBefore(i,l)})(window,document,'script','https://collector.ecomlogic.com/w.js','_rc');
+
+		_rc('create', 'RC-68376489030-3');
+		_rc('send', 'pageView');
+	</script>
+</head>
+<body style="background-color: #E4E4E4;">
+	<script type="text/javascript">
+		
+		_rc('require', 'capture-form', {
+			'period': 0,
+			'fields': {
+				'name': {required: true, label: 'Имя'},
+				'email': { required: true, label: 'Email'},
+				'customerComment': {required: true, label: 'Задать вопрос'}
+			},
+			labelPromo: "Остались вопросы?",
+			labelSend: "Свяжитесь со мной"
+
+
+		});
+	</script>
+
+	<div class="fields">
+		<div>
+			<nav class="navbar  navbar-expand-lg rounded-4 my-5 px-3 bg-black " data-bs-theme="dark">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">
+						<p class="fw-bold text-info m-0 px-2 fs-5 my-1" >in<span class="text-white">WEB</span></p>		</a>
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarNav">
+							<ul class="navbar-nav">
+
+								<li class="nav-item">
+									<a class="nav-link text-white btnExecute px-3 mx-2 fw-bold fs-5 my-1" href="#WEB">WEB для меня</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white btnExecute fw-bold px-3 mx-2 fs-5 my-1" href="#form">Связаться со мной</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+
+			
+			
+			<div class="bloc-1 align-items-center">
+
+				<div>
+					<p class="name" >
+						Пустовалова Ирина
+					</p>
+					<p class="text-primary pb-2 web">
+						WEB-разработка					
+					</p>
+
+					<p class="phone text-ssecondary pt-2">
+						<i class='fa fa-phone'></i>
+						&ensp;+7 904 289 60 23
+					</p>
+					<p class="mail text-ssecondary">
+						<i class='fa fa-envelope'></i>
+						&ensp;iraledy@yandex.ru
+					</p>
+
+					<a href="#form">
+						<input type="button" id="submit" class="btn btn-outline-primary  fw-bold border-3 btn-write fs-4 my-5" value="Написать" >
+						
+					</a>
+					
+					
+				</div>
+				<img class="bg-img rounded-4 mb-5" src="image-v4-1m.png">
+				<img class="bg-img-1 rounded-4" src="image-v4mm.png">
+
+			</div>
+			<div class="bg-primary rounded-5 bloc-2">
+				<div class="pt-3  pb-2">
+					<h1 class="text-white pt-5 text-center fw-bold h" id="WEB">
+						Web  для меня - это...
+					</h1>
+				</div>
+
+				<div class="row pt-5 px-2">
+					<div class="col-lg-6 col-xxl-4 pb-5 mb-3 mb-sm-0 d-flex justify-content-center">
+						<div class="card h-100 mb-3 p-3 mx-2 bg-white rounded-4" style="max-width: 46rem;">
+							<div class="card-body">
+								<img src="Asset 71.png" class="pb-3" style="width : 30%" >
+								<h2 class="card-title mb-3">Творчество</h2>
+								<p class="card-text c-t">Одно и тоже можно сделать по-разному. Формы, цвета, шрифты - соединяя их, получается красота.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-xxl-4 mb-3 pb-5 mb-sm-0 d-flex justify-content-center">
+						<div class="card h-100 mb-3 p-3 mx-2 bg-white rounded-4" style="max-width: 46rem;">
+							<div class="card-body">
+								<img src="Asset 111.png" class="pb-3" style="width : 30%" >
+								<h2 class="card-title mb-3">Взаимодействие</h2>
+								<p class="card-text c-t">Понимаю, что одним важно в точности отрисовать дизайн, а другие просят совета. Общаясь, мы договоримся, как действовать. </p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-xxl-4 mb-3 pb-5 mb-sm-0 d-flex justify-content-center">
+						<div class="card h-100 mb-3 p-3 mx-2 bg-white rounded-4" style="max-width: 46rem;">
+							<div class="card-body">
+								<img src="Asset 61.png" class="pb-3" style="width : 30%" >
+								<h2 class="card-title mb-3">Проектная работа</h2>
+								<p class="card-text c-t">Любой заказ - это проект. За одной страницей спрятано множество элементов, которые образуют сложную структуру.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-xxl-4 mb-3 pb-5 mb-sm-0 d-flex justify-content-center">
+						<div class="card h-100 mb-3 p-3 mx-2 bg-white rounded-4" style="max-width: 46rem;">
+							<div class="card-body">
+								<img src="Asset 91.png" class="pb-3" style="width : 30%" >
+								<h2 class="card-title mb-3">Навык -> Чудо</h2>
+								<p class="card-text c-t">Это когда использую код (html, css, php, js) и этот код творит чудеса: появляются картинки, отправляются данные, сохраняется или отображается информация.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-xxl-4 mb-3 pb-5 mb-sm-0 d-flex justify-content-center">
+						<div class="card h-100 mb-3 p-3 mx-2 bg-white rounded-4" style="max-width: 46rem;">
+							<div class="card-body">
+								<img src="Asset 101.png" class="pb-3" style="width : 30%" >
+								<h2 class="card-title mb-3">Командная работа</h2>
+								<p class="card-text c-t">Мы... В этом слове так много. Мы создаем продукт - с вами мечтаю работать в команде, в которой будем вместе творить нечто большее.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-xxl-4 mb-3 pb-5 mb-sm-0 d-flex justify-content-center">
+						<div class="card h-100 mb-3 p-3 mx-2 bg-white rounded-4" style="max-width: 46rem;">
+							<div class="card-body">
+								<img src="Asset 21.png" class="pb-3" style="width : 30%" >
+								<h2 class="card-title mb-3">Развитие</h2>
+								<p class="card-text c-t">Чтобы реализовать эту страницу нужно было изучить так много. А так много еще предстоит узнать и внедрить. Развитие внутренних качеств, а также профессиональных.</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="bloc-3 align-items-center">
+				<img class="bg-img-2 col-12 col-xl-6 rounded-4" src="image-v5mm.png">
+				<div class="rounded-5 col-12 col-xl-6 py-4 feedback" >
+				<h1 class="py-4 text-center hh fw-bold" id="form">Связаться со мной</h1>
+				<div class="row">
+					
+					<form class="py-3 col-sm-10 offset-sm-1 text-center" autocomplete="off" action="email.php" id='feedback-form' method='post'>
+						<?php
+						if(!empty($_GET['message'])) {
+							$message = $_GET['message'];
+
+							if($message == 'Отправлено.')
+							{
+								echo '<div class="alert alert-success alert-link" role="alert">
+								'.$message.'
+								</div>';
+							}
+							else
+							{
+								echo '<div class="alert alert-danger alert-link" role="alert">
+								Ошибка при отправке формы.
+								</div>';
+
+							}
+						}
+						?> 						
+						<div class="form-group">
+							<input class="form-control  py-2 px-4 mt-2 fs-4" type='text' name='name' pattern="[\S\s]*[a-zA-Zа-яёА-ЯЁ]+[\S\s]*" required title="Имя должно содержать символы помимо пробела" placeholder="Введите свое имя">
+						</div>
+						<div class="form-group">
+							<input class="form-control  py-2 px-4 mt-4 fs-4" type='email' name='email' required placeholder="Введите адрес электронной почты" title="Введите email в формате ira@mail.com. Email может содержать только латинские буквы и цифры"pattern="[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+">
+						</div>
+						<div class="form-group">
+							<textarea class="form-control  py-2 px-4 mt-4 fs-4" id="exampleFormControlTextarea1" name="feedback" rows="3" required placeholder="Ваше сообщение" ></textarea>
+						</div>
+						<input type="submit" class="btn btn-primary px-4 my-5  fw-bold fs-4"  id="submit" placeholder="Отправить">
+
+					</form>
+
+				</div>
+
+
+			</div>
+			</div>
+
+
+		</div>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script type="text/javascript" src="/jquery.url.js"></script>
+	</body>
+	</html> 
